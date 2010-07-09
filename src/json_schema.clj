0b86f9aa062a43feb1b33a76a83ce5c2928964f9
Pedro Henriques dos Santos Teixeira <pedro.t@gmail.com>
2010-07-09 13:47:11 -0300

    first commit

--- src/json_schema.clj
+++ src/json_schema.clj
@@ -2,25 +2,86 @@
   "This is JSON Schema in Clojure. See http://tools.ietf.org/html/draft-zyp-json-schema-02
  Main purposed is to allow object validation, but schema metadata can be used for exposing contracts as well."
   (:use clojure.walk
+        clojure.template
         clojure.contrib.condition
-        (clojure.contrib [ns-utils :only (immigrate)])))
-
-
-;; (defprotocol Schema
-;;   (validate [this data]))
-;extend for String, for Map and for Array?
+        (clojure.contrib [ns-utils :only (immigrate)]))
+  (:require [clojure.set :as set]))
 
 
 
-(defmacro check
-  "raise, keep context to output various validations"
-  [condition & args]
-  (let [key (or (first args) :generic)]
-    `(or ~condition
-         (raise :type ~key))))
+(def *schema-context* nil)
+(def *parent* nil)
 
 
-(def type-validations
+(defn process-errors
+  "Default processing is just output boolean return."
+  [errors]  
+  (= (count errors) 0))
+
+(defmacro with-validation-context  
+  "Defines a binding to allow access to the root object and to enable invalidations to be captured. This strategy removes the need of raising exceptions at every single invalid point, and allows context information to be used when reporting about errors."
+  [& body]  
+  `(let [body# #(do ~@body
+                    (process-errors @(:errors *schema-context*)))]
+     (if-not *schema-context*  
+       (binding [*schema-context* {:errors (ref '())
+                                   :path (ref [])}]                  
+         (body#))
+       (body#))))
+
+
+(defmacro walk-in
+  [parent rel-path & body]
+  `(binding [*parent* ~parent]
+     (if-let [{path# :path} *schema-context*]
+       (do 
+         (dosync alter path# conj ~rel-path)
+         ~@body
+         (dosync alter path# pop))
+       (do ~@body))))
+
+
+(defmacro invalid
+  ""
+  [& args]
+  (let [[path args] (if (keyword? (first args)) [nil args] [(first args) (rest args)])
+        key (first args)
+        data (second args)] 
+    `(let [error# {:ref ~path :key ~key :data ~data}]
+       (if-let [{errors# :errors path# :path} *schema-context*]
+         (dosync (alter errors# conj
+                        (merge {:path (conj @path# ~path)} error#))))
+       (process-errors (list error#)))))
+
+
+(defmacro report
+  "Returns all errors."
+  [& args]
+  `(binding [process-errors (fn [errors#] errors#)]
+     (with-validation-context    
+       (do ~@args))))
+
+
+(defmulti validate*
+  "Dispatch on object type for validation. If not implemented, performs only basic type validation."
+  (fn [schema instance] (:type schema)))
+
+
+(defn validate [schema instance]
+  (with-validation-context
+    (validate* schema instance)))
+
+
+
+(def default-type "object")
+
+
+(defmethod validate* nil [schema instance]
+  (validate (merge schema {:type default-type}) instance))
+
+
+(def ^{:doc "Known basic types."}
+     basic-type-validations
      { "object" #(map? %)
        "array" #(or (vector? %) (list? %))
        "string" #(string? %)
@@ -31,45 +92,108 @@
        "any" (fn [] true)})
 
 
-(defn validate-type
-  "Single type definition or a collection indicating a union of type definitions."
-  [schema data]
-  (let [type (:type schema)
-        type (if (coll? type) type (vector type))]
-    (check (reduce #(or %1 %2)
-                   (map (fn [t] ((type-validations t) data)) type))
-           :invalid-type {:expected "object" :actual (type data)})))
-
-
-
-(defmulti validate
-  "Dispatch on object type for validation. If not implemented, performs only type validation."
-  (fn [schema data] (:type schema)))
-
-
-(defmethod validate "object" [schema data]
-  (do 
-    (validate-type schema data)
+(defn check-basic-type
+  "Validate basic type definition for known types."
+  [{t :type :as schema} instance]
+  (let [t (or t default-type)
+        types (if (coll? t) t (vector t))]
     
-    (for [[property-name property-schema] (:properties schema)]
-      (let [property (data property-name)]
-        (check (or property
-                   (:optional property-schema)))
-
-        (when (:requires property-schema)
-          (check (data (keyword (:requires property-schema)))))
-                
-        (validate property-schema property)))))
+    (or (reduce #(or %1 %2)
+                (map (fn [t] ((basic-type-validations t) instance)) types))
+        (invalid :type {:expected types :actual (type instance)}))))
 
 
-(defmethod validate nil [schema data]
-  (validate (merge schema {:type "object"}) data)) 
+(defn common-validate [schema instance]
+  (check-basic-type schema instance)
+
+;enum
+;default  
+  )
 
 
-(defmethod validate "array" [schema data])
+(defmethod validate* :default [schema instance]
+  (common-validate schema instance))
 
 
-(defmethod validate :default [schema data]
-  (do 
-    (validate-type schema data)))
+(defmethod validate* "object"
+  [{properties-schema :properties
+    additional-schema :additionalProperties
+    :as schema} instance]
 
+  (common-validate schema instance)
+
+  #_ "validate properties defined in schema"
+  (doseq [[property-name
+           {optional :optional :as property-schema}] properties-schema]    
+    (let [property (instance property-name)]
+      (when-not (or property optional)
+        (invalid property-name :required)))) 
+  
+
+  #_ "validate instance properties (using invidivual or addicional schema)"
+  (doseq [[property-name property] instance]
+    (if-let [{requires :requires :as property-schema}
+             (or (and (map? properties-schema) (properties-schema property-name))
+                 (and (map? additional-schema) additional-schema))]
+      (do        
+        (when (and requires property
+                   (not (instance (keyword requires))))
+          (invalid requires :required {:required-by property-name}))
+      
+        (walk-in instance property-name
+                 (validate property-schema property)))))
+
+    
+  #_ "check additional properties"
+  (when (false? additional-schema)
+    (if-let [additionals (set/difference (set (keys instance))
+                                      (set (keys properties-schema)))]
+      (when (> (count additionals) 0)
+        (invalid :addicional-properties-not-allowed {:properties additionals}))))) 
+
+
+
+
+(defmethod validate* "array"
+  [{items-schema :items
+    unique? :uniqueItems :as schema} instance]
+  
+  (common-validate schema instance)
+
+  #_ "specific array validation"
+  (let [total (count instance)]
+    (do-template
+     [key op]
+     (if-let [expected (key schema)]
+       (when (and (op total expected))
+         (invalid key {:expected expected :actual total}))
+       :minItems <
+       :maxItems >)))
+
+  (if-let [unique? (:uniqueItems schema)]
+    (reduce (fn [l r] (when-not (= l r)
+                        (invalid :uniqueItems {:l l :r r}))) instance))
+  
+  
+  #_ "treat array as object for further common validation"
+  (when items-schema
+    (let [obj-array (zipmap (range (count instance)) instance)
+          obj-schema (cond (and (map? items-schema)(:type items-schema))
+                           {:type "object"
+                            :additionalProperties items-schema}
+                           
+                           (coll? items-schema)
+                           (merge schema {:type "object"
+                                          :properties (zipmap (range (count items-schema)) items-schema)}))]
+      (validate obj-schema obj-array))))
+
+
+
+
+(defmethod validate* "string"
+  [schema instance]
+  (common-validate schema instance)
+;minLength
+;maxLength
+;pattern  
+  )
--- test/test_json_schema.clj
+++ test/test_json_schema.clj
@@ -1,46 +1,127 @@
 (ns test-json-schema
   (:use json-schema
-	lazytest.describe))
+	lazytest.describe))  
+
+
+(defmacro ctx
+  "Use simple value contexts with the :given options."
+  [v] `(context [] ~v))
  
 
-(defmacro raised?
+(defmacro raised? 
   "Returns true if body raises a condition that matches with (using the keys of) the expected condition in the argument."
   [condition & body]
   `(try ~@body false
         (catch clojure.contrib.condition.Condition e#
-          (let [actual# (select-keys (meta e#) (keys ~condition))]
-            (= actual# ~condition)))))
-
-
-(describe validate-type
-  (it "should validate based on a single type"
-    (validate-type {:type "object"} {}))
-
-  (it "should perform union validation when multiple types are defined"
-    (validate-type {:type ["object" "array"]} [])
-    (validate-type {:type ["object" "array"]} {})) 
-
-  (it "should raise condition if object does not match schema"
-    (raised? {:type :invalid-type} (validate-type {:type "object"} [])))) 
+          (or (= ~condition :any) 
+              (do (assert (map? ~condition))
+                  (let [actual# (select-keys (meta e#) (keys ~condition))]
+                    (= actual# ~condition)))))))
 
 
 
-(def product-schema
-     {:name "Product"
-      :properties {:id    {:type "number" :description "Product identifier"}
-                   :name  {:description "Name of the product" :type "string"}
-                   :price {:type "number" :minimum 0}
-                   :tags  {:optional true :type "array" :items {:type "string"}}}})
+(def base-schema {:type "object"
+                         :properties {:id    {:type "number"}
+                                      :name  {:type "string"}
+                                      :description {:optional true :type "string"}}})
+ 
+(describe validate "object"
+    
+  (describe "with properties schema"
+    :given [s (ctx base-schema)]
+ 
+    (it "should accept object with only required properties"
+      (validate s {:id 1 :name "shoe"})) 
 
-(def p1 {:id 1 :name "Shoe" :price 400})
+    (it "should accept object with optional properties"
+      (validate s {:id 1 :name "shoe" :description "style"}))
+    
+    (it "should not accept when required property is not present" 
+      (not (validate s {:id 1})))
+    
+    (it "should not accept when property type is incorrect"
+      (not (validate s {:id "1" :name "bag"}))))
+  
+   
+  (describe "with additional properties schema"
 
-(describe validate "with correct simple object"
-  (it "should return true"
-    (validate product-schema p1)))
+    (describe "set to false"
+      :given [s (ctx (merge base-schema {:additionalProperties false}))]
+
+      (it "should not allow any properties that are not defined in schema"
+        (not (validate s {:id 1 :name "wine" :age "13 years"}))))
+
+    (describe "defining a schema"
+      :given [s (ctx (merge base-schema {:additionalProperties {:type "string"}}))]
+
+      (it "should enforce that all extra properties conform to the schema"
+        (and 
+         (validate s {:id 1 :name "wine" :age "13 years" :country "france"})
+         (not (validate s {:id 1 :name "wine" :age 13})))))))
 
 
-(describe validate "array"
-  (it ""))
 
+(describe validate "items"
 
+  (describe "with no items definition"
+    (it "should allow any"
+      (and 
+       (validate {:type "array"} [ 1 2 3])
+       (validate {:type "array" :items []} [1 "2" 3]))))
+  
+  (describe "with object schema" :given [s1 (ctx {:type "array" :items {:type "string"}})
+                                         s2 (ctx {:type "array" :items {:type "object" :properties {:name {:type "string"}}}})]     
+    (it "should accept empty array"
+      (validate s1 []))
+    
+    (it "should accept homogenous array"
+      (and  
+       (validate s1 ["a" "b" "c"])
+       (validate s2 [{:name "half"} {:name "life"}])))
+ 
+    (it "should not accept heterogenous array"
+      (not (validate s1 ["a" "b" 3])))
 
+    (it "should not accept if inner item does not follow item schema" 
+      (not (validate s2 [{:name "quake"} {:id 1}]))))
+  
+ 
+  
+  (describe validate "with array of schemas"      
+      
+    (describe "with single object definitions"
+      :given [s (ctx {:type "array" :items [{:type "string"} {:type "string"}]})]
+      
+      (it "should enforce tuple typing"
+        (and  
+         (validate s ["a" "b"])
+         (not (validate s ["a"]))
+         (not (validate s ["a" 1]))))
+
+      (it "should allows additional properties to be defined by default"
+        (and 
+         (validate s ["a" "b" "c"])
+         (validate s ["a" "b" 1 2 3 {}])))
+
+      )
+
+    (describe "with additional properties bounded"
+      :given [s (ctx {:type "array" :items [{:type "number"}] :additionalProperties {:type "string"}})]
+
+      (it "should ensure type schema for additional properties"
+        (and 
+         (validate s [1 "a" "b" "c"])
+         (not (validate s [1 2 "a"]))))
+
+      (it "should still enforce tuple typing"
+        (not (validate s ["a" 1])))) 
+
+      
+    (describe "with aditional properties disabled"
+      :given [s (ctx {:type "array" :items [{:type "number"}] :additionalProperties false})] 
+      
+      (it "should be strict about tuple typing"
+        (validate s [1])) 
+
+      (it "should be strict about tuple typing" 
+        (not (validate s [1 "a"]))))))
